7c569f48066514e7ac0b7834a31580bbea24da40
Søren Sejr Olsen <soerensejr@yahoo.dk>
2024-11-16 18:10:31 +0100

    adding all files

---
+++ src/main/java/com/example/lombok_annotations_error/LombokAnnotationsErrorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lombok_annotations_error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokAnnotationsErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokAnnotationsErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lombok_annotations_error/LombokAnnotationsErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lombok_annotations_error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokAnnotationsErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
